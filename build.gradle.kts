ea15452b846195d25c533c22f2bb2bac258f3851
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2019-03-18 09:01:33 +0100

    upgrade plugins and remove unused import

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     dependencies {
-        //must specify this in gradle.properties since the same version must be here and in aurora plugin
+        // must specify this in gradle.properties since the same version must be here and in aurora plugin
         val springCloudContractVersion: String = project.property("aurora.springCloudContractVersion") as String
         classpath("org.springframework.cloud:spring-cloud-contract-gradle-plugin:$springCloudContractVersion")
     }
@@ -9,7 +9,7 @@ buildscript {
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.21"
     id("org.jetbrains.kotlin.plugin.spring") version "1.3.21"
-    id("org.jlleitschuh.gradle.ktlint") version "7.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "7.2.1"
 
     id("org.springframework.boot") version "2.1.3.RELEASE"
     id("org.asciidoctor.convert") version "1.6.0"
@@ -18,14 +18,12 @@ plugins {
     id("com.github.ben-manes.versions") version "0.21.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.9"
 
-    id("no.skatteetaten.gradle.aurora") version "2.0.2"
-
+    id("no.skatteetaten.gradle.aurora") version "2.2.0"
 }
 
 apply(plugin = "spring-cloud-contract")
 
 dependencies {
-    val springCloudContractVersion: String = project.property("aurora.springCloudContractVersion") as String
     implementation("io.fabric8:openshift-client:4.1.3")
     testImplementation("com.fkorotkov:kubernetes-dsl:2.0.1")
     testImplementation("com.squareup.okhttp3:mockwebserver:3.14.0")
@@ -35,7 +33,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner:$springCloudContractVersion")
 
     testImplementation("io.mockk:mockk:1.9.2")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.13")
