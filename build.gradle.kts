e0d718521e36764754abf6735a30f8d5451c47ec
Frode Mathiesen <Frode.Mathiesen@skatteetaten.no>
2021-11-15 12:19:30 +0100

    AOS-4564: updating dependencies and set iqBreakOnUnstable

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,20 @@
 plugins {
-    id("org.springframework.cloud.contract")
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.72"
-    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
-    id("org.sonarqube") version "3.0"
+    id("java")
+    id("no.skatteetaten.gradle.aurora") version "4.3.22"
+}
 
-    id("org.springframework.boot") version "2.2.6.RELEASE"
-    id("org.asciidoctor.convert") version "2.4.0"
-
-    id("com.gorylenko.gradle-git-properties") version "2.2.2"
-    id("com.github.ben-manes.versions") version "0.28.0"
-    id("se.patrikerdes.use-latest-versions") version "0.2.14"
-
-    id("no.skatteetaten.gradle.aurora") version "3.5.2"
+aurora {
+    useKotlinDefaults
+    useSpringBootDefaults
+    useAsciiDoctor
+    useSpringBoot {
+        useCloudContract
+    }
 }
 
 dependencies {
-    implementation("io.fabric8:openshift-client:4.10.2")
-    testImplementation("com.fkorotkov:kubernetes-dsl:2.8")
+    implementation("io.fabric8:openshift-client:5.9.0")
+    testImplementation("com.github.fkorotkov:k8s-kotlin-dsl:3.0.1")
 
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -25,8 +22,12 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    testImplementation("io.mockk:mockk:1.10.0")
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.22")
-    testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:1.1.0")
-    testImplementation("com.ninja-squad:springmockk:2.0.1")
+    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
+    testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:1.1.7")
+    testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.2.0")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
+}
+repositories {
+    mavenCentral()
 }
