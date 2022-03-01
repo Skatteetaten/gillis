85ef03256edb5e4fb48b15cdf52da565e285ec9f
Martin Rodland <Martin.Rodland@skatteetaten.no>
2022-03-01 08:46:31 +0100

    temp removing EnableWebFlux

--- src/main/kotlin/no/skatteetaten/aurora/gillis/Main.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gillis/Main.kt
@@ -2,9 +2,7 @@ package no.skatteetaten.aurora.gillis
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.web.reactive.config.EnableWebFlux
 
-@EnableWebFlux
 @SpringBootApplication
 class Main
 
