41b2e5d9903702b1d13324d3a9876bd9c1022a12
Frode Mathiesen <Frode.Mathiesen@skatteetaten.no>
2022-08-22 12:35:41 +0200

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("no.skatteetaten.gradle.aurora") version "4.4.10"
+    id("no.skatteetaten.gradle.aurora") version "4.5.4"
 }
 
 aurora {
@@ -13,29 +13,29 @@ aurora {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("io.fabric8:openshift-client:5.10.1")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("io.fabric8:openshift-client:6.0.0")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.7")
     testImplementation("com.github.fkorotkov:k8s-kotlin-dsl:3.0.1")
     implementation("io.projectreactor:reactor-tools")
-    implementation("no.skatteetaten.aurora.kubernetes:kubernetes-reactor-coroutines-client:1.3.26")
+    implementation("no.skatteetaten.aurora.kubernetes:kubernetes-reactor-coroutines-client:1.3.31")
 
     /* explicit okhttp3 dependencies to force transitive */
-    implementation("com.squareup.okhttp3:okhttp:4.9.3")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.2")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
 
     implementation("org.springframework.boot:spring-boot-starter-security")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    testImplementation("io.mockk:mockk:1.12.1")
+    testImplementation("io.mockk:mockk:1.12.5")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
     testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.1.8") {
         exclude(group = "no.skatteetaten.aurora.springboot", module = "aurora-spring-boot-mvc-starter")
     }
-    testImplementation("com.ninja-squad:springmockk:3.0.1")
-    testImplementation("org.junit-pioneer:junit-pioneer:1.5.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
+    testImplementation("org.junit-pioneer:junit-pioneer:1.7.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
     testImplementation("io.projectreactor:reactor-test")
 }
