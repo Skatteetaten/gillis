c06f4c9c06825bdf3afe3ca47532b01a9fd30db7
Frode Mathiesen <Frode.Mathiesen@skatteetaten.no>
2022-08-22 14:04:36 +0200

    remove dependencies no longer needed or coming as transitive dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,24 +16,17 @@ aurora {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("io.fabric8:openshift-client:6.0.0")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.7")
-    testImplementation("com.github.fkorotkov:k8s-kotlin-dsl:3.0.1")
-    implementation("io.projectreactor:reactor-tools")
     implementation("no.skatteetaten.aurora.kubernetes:kubernetes-reactor-coroutines-client:1.3.31")
-
     implementation("org.springframework.boot:spring-boot-starter-security")
+
+    testImplementation("com.github.fkorotkov:k8s-kotlin-dsl:3.0.1")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     testImplementation("io.mockk:mockk:1.12.5")
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
     testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.3.1")
 
     testImplementation("com.ninja-squad:springmockk:3.1.1")
-    testImplementation("org.junit-pioneer:junit-pioneer:1.7.1")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
     testImplementation("io.projectreactor:reactor-test")
 }
