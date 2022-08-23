fe3747a8dbc45fc0796e8cabed176d88df8a3453
furode <88827279+furode@users.noreply.github.com>
2022-08-23 13:54:11 +0200

    Aos 6911 nexusiq (#14)
    * upgrade dependencies
    
    * upgrade to java 17
    
    * remove dependencies forcing transitive version
    
    * add spring.application.name
    
    * remove dependencies no longer needed or coming as transitive dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("no.skatteetaten.gradle.aurora") version "4.4.10"
+    id("no.skatteetaten.gradle.aurora") version "4.5.4"
 }
 
 aurora {
@@ -10,32 +10,23 @@ aurora {
     useSpringBoot {
         useWebFlux
     }
+    versions {
+        javaSourceCompatibility = "17"
+    }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("io.fabric8:openshift-client:5.10.1")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.5")
-    testImplementation("com.github.fkorotkov:k8s-kotlin-dsl:3.0.1")
-    implementation("io.projectreactor:reactor-tools")
-    implementation("no.skatteetaten.aurora.kubernetes:kubernetes-reactor-coroutines-client:1.3.26")
-
-    /* explicit okhttp3 dependencies to force transitive */
-    implementation("com.squareup.okhttp3:okhttp:4.9.3")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.2")
-
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.7")
+    implementation("no.skatteetaten.aurora.kubernetes:kubernetes-reactor-coroutines-client:1.3.31")
     implementation("org.springframework.boot:spring-boot-starter-security")
+
+    testImplementation("com.github.fkorotkov:k8s-kotlin-dsl:3.0.1")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    testImplementation("io.mockk:mockk:1.12.1")
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
-    testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.1.8") {
-        exclude(group = "no.skatteetaten.aurora.springboot", module = "aurora-spring-boot-mvc-starter")
-    }
-    testImplementation("com.ninja-squad:springmockk:3.0.1")
-    testImplementation("org.junit-pioneer:junit-pioneer:1.5.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
+    testImplementation("io.mockk:mockk:1.12.5")
+    testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.3.1")
+
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
     testImplementation("io.projectreactor:reactor-test")
 }
